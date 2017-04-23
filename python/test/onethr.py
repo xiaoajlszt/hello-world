f2ec8d24c4b762cafac91eb08e3ac0454b636510
*** <***>
2017-04-24 00:19:10 +0800

    0423

---
+++ python/test/onethr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from time import sleep,ctime
+
+def loop0():
+    print 'start loop0 at:', ctime()
+    sleep(4)
+    print 'loop0 done at:', ctime()
+
+def loop1():
+    print 'start loop1 at:', ctime()
+    sleep(2)
+    print 'loop1 done at:', ctime()
+
+def main():
+    print 'starting at:', ctime()
+    loop0()
+    loop1()
+    print 'all done at:', ctime()
+
+if __name__ == '__main__':
+    main()
