60cfb787ecb2eaae9741598219a897d84188f468
xiao.peng <xiao.peng77@zte.com.cn>
2017-05-19 18:17:04 +0800

    0519

---
+++ python/test/第2章/tsTclnt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = '127.0.0.1' # or 'localhost'
+PORT = 21567
+BUFSZIE = 1024
+ADDR = (HOST,PORT)
+
+tcpCliSock = socket(AF_INET,SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSZIE)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
